1f6b8b91c52b733f4eca6b008d16f170e79a8351
Jose Polanco <lanjos26@gmail.com>
2022-10-12 23:18:24 -0400

    added code x2

--- get_weather_fn.cs
+++ get_weather_fn.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Threading.Tasks;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Host;
 using Microsoft.Extensions.Logging;
@@ -8,9 +9,30 @@ namespace weather_daily_github_fn
     public class get_weather_fn
     {
         [FunctionName("get_weather_fn")]
-        public void Run([TimerTrigger("0 */5 * * * *", RunOnStartup = true)]TimerInfo myTimer, ILogger log)
+        public async Task Run([TimerTrigger("0 */5 * * * *", RunOnStartup = true)]TimerInfo myTimer, ILogger log)
         {
             log.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
+            var weatherApiKey = Environment.GetEnvironmentVariable("weatherApiKey");
+            var emailAuthPassword = Environment.GetEnvironmentVariable("gmailpasswd");
+            if(weatherApiKey == null || emailAuthPassword == null)
+            {
+                log.LogError("Could not retrieve any of the keys.");
+                return;
+            }
+
+            try
+            {
+                var openWeatherAPI = new OpenWeatherAPI.OpenWeatherApiClient(weatherApiKey);
+                var query = await openWeatherAPI.QueryAsync("New York,us");
+                var message = "The weather is " + query.Main.Temperature.CelsiusCurrent + " Celcius \n At: " + DateTime.Now + "\n For: " +
+                    query.Sys.Country;
+                log.LogInformation("Weather API called succesfully");
+
+            }
+            catch (Exception e)
+            {
+                log.LogError(e.Message);
+            }
         }
     }
 }
